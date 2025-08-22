45e4ce09c2b4e39dc3fe11dfc0fbcf187eb6f863
TotalPay <info@totalpay.global>
2025-08-22 10:52:23 +0400

    Update hash_util.dart

--- totalpay_checkout_sdk/lib/src/utils/hash_util.dart
+++ totalpay_checkout_sdk/lib/src/utils/hash_util.dart
@@ -4,6 +4,7 @@ import 'package:totalpay_checkout_sdk/src/models/payment_request.dart';
 import 'package:totalpay_checkout_sdk/src/totalpay_sdk.dart';
 
 class HashUtil {
+  /// Standard hash: SHA1(MD5(order_number + amount + currency + description + password).upper)
   static String generateHash({
     required PaymentRequest request,
     required String password,
@@ -19,22 +20,20 @@ class HashUtil {
 
     final md5Digest = md5.convert(utf8.encode(raw)).toString();
     TotalPaySdk().debugLog("MD5 Hash: $md5Digest");
-    //print("MD5 Hash: $md5Digest");
 
     final sha1Digest = sha1.convert(utf8.encode(md5Digest)).toString();
     TotalPaySdk().debugLog("Final SHA1 Hash: $sha1Digest");
-    //print("Final SHA1 Hash: $sha1Digest");
 
     return sha1Digest;
   }
 
+  /// Recurring hash: SHA1(MD5(transId + token + order.number + order.amount + order.description + password).upper)
   static String generateRecurringHash({
-  required String transId,
-  required String token,
-  required Order order,
-  required String password,
+    required String transId,
+    required String token,
+    required Order order,
+    required String password,
   }) {
-    // Step 1: Concatenate in correct order
     final raw = (transId +
             token +
             order.number +
@@ -42,22 +41,19 @@ class HashUtil {
             order.description +
             password)
         .toUpperCase();
-  
+
     TotalPaySdk().debugLog("Recurring raw string: $raw");
-  
-    // Step 2 + 3: MD5 hash
+
     final md5Digest = md5.convert(utf8.encode(raw)).toString();
     TotalPaySdk().debugLog("Recurring MD5 Hash: $md5Digest");
-  
-    // Step 4: SHA1 of MD5
+
     final sha1Digest = sha1.convert(utf8.encode(md5Digest)).toString();
     TotalPaySdk().debugLog("Recurring SHA1 Hash: $sha1Digest");
-  
+
     return sha1Digest;
   }
 
-  
-  /// Hash for getTransactionStatus
+  /// Transaction status hash: SHA1(MD5(transactionId + password).upper)
   static String generateStatusHash({
     required String transactionId,
     required String password,
